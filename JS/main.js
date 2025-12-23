b865ad15c38169c7fe33f189d0c6409f228f7f77
MarGuz <137.marguz@gmail.com>
2025-12-23 18:08:38 -0300

    refactor con sass y BEM

--- JS/main.js
+++ JS/main.js
@@ -4,16 +4,22 @@ function loadCities() {
     cities.forEach(({ region, city }) => {
 
         const col = document.createElement("div");
-        col.className = "col-12 col-sm-6 col-md-4 col-lg-3";
+        col.className = "col-12 col-sm-6 col-md-4 col-lg-3 weather__col";
 
         col.innerHTML = `
-            <div class="card shadow-sm h-100">
-                <div class="card-body text-center">
-                    <h5 class="card-title">${city}</h5>
-                    <p class="card-text text-muted">${region}</p>
-                    <button class="btn btn-primary btn-sm ver-detalle">Ver detalle</button>
+            <article class="card shadow-sm h-100 place-card">
+                <div class="card-body text-center place-card__body">
+                    <h5 class="card-title place-card__title">
+                        ${city}
+                    </h5>
+                    <p class="card-text text-muted place-card__region">
+                        ${region}
+                    </p>
+                    <button class="btn btn-primary btn-sm ver-detalle place-card__button">
+                        Ver detalle
+                    </button>
                 </div>
-            </div>
+            </article>
         `;
 
         col.querySelector(".ver-detalle").addEventListener("click", () => {
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # weather-frontend-m2
 app de clima
+## 🔗 Repositorio en GitHub
+https://github.com/LaMarGuz/weather-frontend-m2
